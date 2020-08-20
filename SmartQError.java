50c28a7384ff5070475f54edf7ec81417e9d993b
Squarenet <46465050+SquarenetDev@users.noreply.github.com>
2020-08-20 11:04:56 +0900

    Update SmartQError.java

--- SmartQError.java
+++ SmartQError.java
@@ -7,10 +7,18 @@ public enum SmartQError {
     CANCEL("취소", 1007),
     FAIL("실패", 1008),
     EXIST_USER("등록된 사용자", 1009),
-    NOT_EXIST_USER("등록되지 않은 사용자", 1010);
-    
-    final private String errorMessage;
-    final private int errorCode;
+    NOT_EXIST_USER("등록되지 않은 사용자", 1010),
+    NO_PARAM("파라미터 없음", 1011),
+    NOT_SUPPORT("지원되지 않음", 1012),
+    NOT_REGIST("등록되지 않음", 1013),
+    UNKNOWN_ERROR("알수 없는 에러", 1014),
+    NOT_REGIST_BIO("생체인증 등록되지 않음", 1015),
+    LOCK_BIO("생체인증 잠김", 1016),
+    ALREADY_REGIST("중복된 등록", 1017),
+    NETWORK_ERROR("네트워크 오류", 1018);
+
+    private String errorMessage;
+    private int errorCode;
 
     SmartQError(String errorMessage, int errorCode) {
         this.errorMessage = errorMessage;
@@ -22,6 +30,12 @@ public enum SmartQError {
     }
     public String getErrorMessage() {
         return errorMessage;
+    }
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage =  errorMessage;
     }
 }
 
