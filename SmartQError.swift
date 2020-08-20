0d930740385fd130e5ae57cf82c89d0eef046f05
Squarenet <46465050+SquarenetDev@users.noreply.github.com>
2020-08-20 11:04:14 +0900

    Update SmartQError.swift

--- SmartQError.swift
+++ SmartQError.swift
@@ -16,7 +16,14 @@ class SmartQError {
     static let  FAIL = ["실패": 1008]
     static let  EXIST_USER = ["등록된 사용자": 1009]
     static let  NOT_EXIST_USER = ["등록되지 않은 사용자": 1010]
-    static let  NO_PARAM = ["파라미터 없음": 1010]
+    static let  NO_PARAM = ["파라미터 없음": 1011]
+    static let  NOT_SUPPORT = ["지원되지 않음": 1012]
+    static let  NOT_REGIST = ["등록되지 않음": 1013]
+    static let  UNKNOWN_ERROR = ["알 수 없는 에러": 1014]
+    static let  NOT_REGIST_BIO = ["생체인증 등록되지 않음": 1015]
+    static let  LOCK_BIO = ["생체인증 잠김": 1016]
+    static let  ALREADY_REGIST = ["중복된 등록": 1017]
+    static let  NETWORK_ERROR = ["네트워크 오류": 1018]
 }
 
 extension Dictionary {
